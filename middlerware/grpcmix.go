22bec2f690edf4da4b976cdc6db9327dfd61c48d
李南希 <leenanxi@gmail.com>
2018-08-10 11:44:03 +0800

    fix grpc mix hanler

--- middlerware/grpcmix.go
+++ middlerware/grpcmix.go
@@ -3,17 +3,41 @@ package middlerware
 import (
 	"net/http"
 	"strings"
+	"golang.org/x/net/http2"
+	"google.golang.org/grpc"
+	"golang.org/x/net/http2/h2c"
 )
 
-const grpcContentType = "application/grpc"
+//GRPCMixOptions the Options of GRPC Mix
+type GRPCMixOptions struct {
+	server *http2.Server
+}
+
+//GRPCMixOption the Option of GRPC Mix
+type GRPCMixOption func(*GRPCMixOptions)
+
+func GRPCMixWithServer(s *http2.Server) GRPCMixOption {
+	return func(o *GRPCMixOptions) {
+		o.server = s
+	}
+}
 
 //GRPCMixHandler mix grpc and http in one Handler
-func GRPCMixHandler(h http.Handler, grpc http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+func GRPCMixHandler(h http.Handler, grpc *grpc.Server, opts ...GRPCMixOption) http.Handler {
+	mix := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		if r.ProtoMajor == 2 && strings.HasPrefix(r.Header.Get("Content-Type"), "application/grpc") {
 			grpc.ServeHTTP(w, r)
 		} else {
 			h.ServeHTTP(w, r)
 		}
 	})
+	var options GRPCMixOptions
+	for _, o := range opts {
+		o(&options)
+	}
+	server := options.server
+	if server == nil {
+		server = &http2.Server{}
+	}
+	return h2c.NewHandler(mix, server)
 }
